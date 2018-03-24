e8abe5ec44562e265da20fb558e8cb126b093616
imkartikahuja <kartik.ahuja24@gmail.com>
2018-03-24 12:44:30 +0530

    Initial commit Add express and logging request

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+//for logging request
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  next();
+});
+
+
+app.listen(3000,() => {
+  console.log('Started on port 3000');
+});
